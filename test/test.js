9f3cc38ec64908c05bf344e8f6add2941c948e65
Dan Gowans <dangowans@users.noreply.github.com>
2023-10-16 09:53:18 -0400

    add more linting errors

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-export function test_function(varName) {
+export function test_fn(varName) {
     // This function is purposely formatted badly to ensure the 
     // ESLint configuration is loading correctly. 
     return varName + ' ' + 2;
--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,11 @@
-export function test_function (varName) {
+
+import fs from 'fs'
+
+export function test_fn (varName) {
 
   // This function is purposely formatted badly to ensure the 
   // ESLint configuration is loading correctly. 
 
   return varName + ' ' + 2
+
 }
