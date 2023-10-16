ae91c46e49d3db412ddaf51194f869897ff2a0ed
Dan Gowans <dangowans@users.noreply.github.com>
2023-10-16 09:53:48 -0400

    add more linting errors

--- test/test.ts
+++ test/test.ts
@@ -6,6 +6,6 @@ export function test_fn (varName) {
   // This function is purposely formatted badly to ensure the 
   // ESLint configuration is loading correctly. 
 
-  return varName + ' ' + 2
+  return varName + ' ' + 2;
 
 }
