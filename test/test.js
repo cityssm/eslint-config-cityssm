f8f63c382ab4585d01632ed277ad4b4089bd03c9
Dan Gowans <dangowans@users.noreply.github.com>
2023-11-01 14:31:49 -0400

    make sure test file hits all plugins

--- test/test.js
+++ test/test.js
@@ -1,5 +1,20 @@
-export function test_fn(varName) {
-    // This function is purposely formatted badly to ensure the 
+/* eslint-disable no-multi-spaces */
+import './missing';
+export async function test_fn(varName) {
+    // This files has purposely formatted badly to ensure the 
     // ESLint configuration is loading correctly. 
-    return varName + ' ' + 2;
+    var VAR = /[0-9\d]/g;
+    return varName + 'test' + ' ' + 2;
+}
+let tst = `test`;
+async function promiseTst(param1, param2, param3, param4, param5, param6) {
+    return new Promise(function (resolve, reject) {
+        for (let i = 1; i <= 1; i++) {
+            //
+        }
+        if (1 == 1)
+            return 'test' + 'test' + 'test' + 'test' + 'test';
+        return 'test' | 'test';
+    }).then((val) => {
+    });
 }
--- test/test.ts
+++ test/test.ts
@@ -1,14 +1,36 @@
 
+/* eslint-disable no-multi-spaces */
+
 import path from 'path'
 import fs from  'fs'
 
-export function test_fn (varName) {
+import './missing'
 
-  // This function is purposely formatted badly to ensure the 
-  // ESLint configuration is loading correctly. 
+
+export async function test_fn (varName) {
+
+  // This files has purposely formatted badly to ensure the 
+   // ESLint configuration is loading correctly. 
 
   var VAR = /[0-9\d]/g
 
-  return varName + ' ' + 2;
+  return varName + 'test' + ' ' + 2;
 
 }
+
+let tst = `test`
+
+async function promiseTst (param1, param2, param3, param4, param5, param6) {
+  return new Promise(function (resolve, reject) {
+    for (let i = 1; i <= 1; i ++) {
+      //
+    }
+    
+    if (1 == 1)
+    return 'test' + 'test' + 'test' + 'test'+ 'test'
+  return 'test' | 'test'
+  }).then((val) => {
+
+  })
+}
+
