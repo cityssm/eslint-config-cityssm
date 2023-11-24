e8715597e9c5ed19afb629f52e9abca78bf9167c
Dan Gowans <dangowans@users.noreply.github.com>
2023-11-24 11:34:52 -0500

    switch execution

--- test/runEslint.mjs
+++ test/runEslint.mjs
@@ -1,25 +1,31 @@
-import { execa } from 'execa';
+import { exec } from 'node:child_process';
 const eslintCommand = 'npx eslint --exit-on-fatal-error ./test/test.ts';
-async function runEslint() {
-    try {
-        await execa(eslintCommand, {
-            stdio: 'inherit',
-            buffer: false
-        });
-        // Process should not finish successfully
-        console.log('❗ No ESLint errors or warnings found.');
-        return 1;
-    }
-    catch (error) {
-        if (error.exitCode === 2) {
+console.log(`Running ${eslintCommand} ...`);
+try {
+    exec(eslintCommand, (error, stdout, stderr) => {
+        if (stdout !== '') {
+            console.log('\nSTDOUT');
+            console.log(stdout);
+        }
+        if (stderr !== '') {
+            console.log('\nSTDERR');
+            console.log(stderr);
+        }
+        if (error === null) {
+            console.log('❗ ESLint did not find any errors or warnings.');
+            process.exitCode = 1;
+        }
+        else if (error.code === 2) {
             console.log('❗ Broken configuration.');
-            return 1;
+            process.exitCode = 1;
         }
         else {
             console.log('✔️ ESLint errors and warnings found. Config checked successfully.');
-            return 0;
+            process.exitCode = 0;
         }
-    }
+    });
 }
-console.log(`Running ${eslintCommand} ...`);
-process.exitCode = await runEslint();
+catch {
+    console.log('❗ Erorr running ESLint.');
+    process.exitCode = 1;
+}
--- test/runEslint.mts
+++ test/runEslint.mts
@@ -1,30 +1,35 @@
-import { type ExecaError, execa } from 'execa'
+import { exec } from 'node:child_process'
 
 const eslintCommand = 'npx eslint --exit-on-fatal-error ./test/test.ts'
 
-async function runEslint(): Promise<number> {
-  try {
-    await execa(eslintCommand, {
-      stdio: 'inherit',
-      buffer: false
-    })
+console.log(`Running ${eslintCommand} ...`)
 
-    // Process should not finish successfully
-    console.log('❗ No ESLint errors or warnings found.')
-    return 1
-  } catch (error) {
-    if ((error as ExecaError).exitCode === 2) {
+try {
+  exec(eslintCommand, (error, stdout, stderr) => {
+    if (stdout !== '') {
+      console.log('\nSTDOUT')
+      console.log(stdout)
+    }
+
+    if (stderr !== '') {
+      console.log('\nSTDERR')
+      console.log(stderr)
+    }
+
+    if (error === null) {
+      console.log('❗ ESLint did not find any errors or warnings.')
+      process.exitCode = 1
+    } else if (error.code === 2) {
       console.log('❗ Broken configuration.')
-      return 1
+      process.exitCode = 1
     } else {
       console.log(
         '✔️ ESLint errors and warnings found. Config checked successfully.'
       )
-      return 0
+      process.exitCode = 0
     }
-  }
+  })
+} catch {
+  console.log('❗ Erorr running ESLint.')
+  process.exitCode = 1
 }
-
-console.log(`Running ${eslintCommand} ...`)
-
-process.exitCode = await runEslint()
