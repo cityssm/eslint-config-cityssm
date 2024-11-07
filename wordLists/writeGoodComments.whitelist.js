1a8708199ede1b63658b0bdd1861f61038ef85b8
Dan Gowans <dangowans@users.noreply.github.com>
2024-11-07 13:03:06 -0500

    generate the capitalized versions

--- wordLists/writeGoodComments.whitelist.js
+++ wordLists/writeGoodComments.whitelist.js
@@ -1,7 +1,15 @@
 // eslint-disable-next-line @eslint-community/eslint-comments/disable-enable-pair
 /* eslint-disable @cspell/spellchecker */
+function capitalize(lowercaseString) {
+    return lowercaseString.charAt(0).toUpperCase() + lowercaseString.slice(1);
+}
+const whitelistLowercase = ['only', 'validate'];
 /**
  * Words that will not be flagged by write-good-comments.
  */
-export const whitelist = ['only', 'validate', 'Validate'];
+export const whitelist = [...whitelistLowercase];
+// List is case-sensitive, include words starting with capitals.
+for (const word of whitelist) {
+    whitelist.push(capitalize(word));
+}
 export default whitelist;
--- wordLists/writeGoodComments.whitelist.ts
+++ wordLists/writeGoodComments.whitelist.ts
@@ -1,9 +1,22 @@
 // eslint-disable-next-line @eslint-community/eslint-comments/disable-enable-pair
 /* eslint-disable @cspell/spellchecker */
 
+function capitalize(lowercaseString: string): string {
+  return lowercaseString.charAt(0).toUpperCase() + lowercaseString.slice(1)
+}
+
+const whitelistLowercase = ['only', 'validate'] satisfies Array<
+  Lowercase<string>
+>
+
 /**
  * Words that will not be flagged by write-good-comments.
  */
-export const whitelist = ['only', 'validate', 'Validate']
+export const whitelist = [...whitelistLowercase] as string[]
+
+// List is case-sensitive, include words starting with capitals.
+for (const word of whitelist) {
+  whitelist.push(capitalize(word))
+}
 
 export default whitelist
