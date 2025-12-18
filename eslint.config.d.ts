fe23460d9e52adb73c73bbe3cae6ce1e30d8109e
Dan Gowans <dangowans@users.noreply.github.com>
2025-12-18 16:18:31 -0500

    version bump

--- eslint.config.d.ts
+++ eslint.config.d.ts
@@ -1,6 +1,6 @@
 /**
  * ESLint Configuration for Web Applications
  */
-export declare const config: any;
+export declare const config: import("eslint/config").Config[];
 export default config;
 export { defineConfig, type Config } from 'eslint/config';
